e6710fe79db8856837d1eec896d0cbe9ee03cc5f
ธีรธร รักษาเมือง <unnie@MacBookAir.local>
2026-01-14 21:47:49 +0700

    feat: create Quest model with name and is_done fields

--- db/migrate/20260114025014_create_quests.rb
+++ db/migrate/20260114025014_create_quests.rb
@@ -1,8 +1,8 @@
 class CreateQuests < ActiveRecord::Migration[8.1]
   def change
     create_table :quests do |t|
-      t.string :title
-      t.text :body
+      t.string :name
+      t.boolean :is_done, default: false
 
       t.timestamps
     end
