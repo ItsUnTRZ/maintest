3ba2afea7fa8def4f2bffd969a1f0aef44d2baf8
ธีรธร รักษาเมือง <unnie@MacBookAir.local>
2026-01-14 22:27:36 +0700

    feat: add create quest functionality

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -8,4 +8,21 @@ class QuestsController < ApplicationController
   def show
     @quest = Quest.find(params[:id])
   end
+
+  # GET /quests/new
+  def new
+    @quest = Quest.new
+  end
+
+  # POST /quests or /quests.json
+  def create
+    @quest = Quest.create(quest_params)
+    redirect_to quests_path
+  end
+
+  private
+
+  def quest_params
+    params.require(:quest).permit(:name)
+  end
 end
