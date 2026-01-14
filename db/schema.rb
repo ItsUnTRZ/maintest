dfddd74783bec3dcbdec76c72714f28836890d4e
ธีรธร รักษาเมือง <unnie@MacBookAir.local>
2026-01-14 22:45:04 +0700

    refactor: Update quests table schema to replace title with name and add is_done field

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_14_025014) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "quests", force: :cascade do |t|
-    t.text "body"
+    t.string "name"
+    t.boolean "is_done", default: false
     t.datetime "created_at", null: false
-    t.string "title"
     t.datetime "updated_at", null: false
   end
 end
