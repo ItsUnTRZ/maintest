efb95b4111d291afea88be7caabbafa2d80fbb16
ธีรธร รักษาเมือง <unnie@MacBookAir.local>
2026-01-14 22:58:59 +0700

    feat: add Brag Document page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :quests
+  get "/brag" => "brag#index", as: :brag
   get "up" => "rails/health#show", as: :rails_health_check
   root "quests#index"
 end
